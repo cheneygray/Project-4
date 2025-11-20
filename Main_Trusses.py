bf7f274f0d6cb7e7d4e2e8f9dc0a2d2e5799ca8e
cheneygray <grayce@byu.edu>
2025-11-19 20:58:43 -0700

    Add files via upload

--- Main_Trusses.py
+++ Main_Trusses.py
@@ -61,11 +61,11 @@ def PlaneTrussStiffness( input_geometry):
         bar.Print()
     
     # Uncomment these for plotting
-    # Plotting_Trusses.PlotStructureData(nodes, bars, "index")
-    # Plotting_Trusses.PlotStructureData(nodes, bars, "axial")
-    # Plotting_Trusses.PlotStructureData(nodes, bars, "stress")
+    Plotting_Trusses.PlotStructureData(nodes, bars, "index")
+    Plotting_Trusses.PlotStructureData(nodes, bars, "axial")
+    Plotting_Trusses.PlotStructureData(nodes, bars, "stress")
     Plotting_Trusses.PlotStructureData(nodes, bars, "disp_in")
-    #Plotting_Trusses.PlotStructureData(nodes, bars, "buckling")
+    Plotting_Trusses.PlotStructureData(nodes, bars, "buckling")
     
     return [nodes,bars]
 
